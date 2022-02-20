e745f20ea1808288433b307dd3ad5ff0f656e1fc
giammacarioca <giammattey@gmail.com>
2022-02-20 09:21:14 -0300

    add RecipeList component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,21 +1,17 @@
 import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 
 // styles
 import './Home.css'
 
 export default function Home() {
-  const {
-    data: recipes,
-    isPending,
-    error,
-  } = useFetch('http://localhost:3000/recipes')
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
 
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
-      {recipes &&
-        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
