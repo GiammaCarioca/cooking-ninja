5a0d52c413b569460010370a5cf47399cc7949fb
giammacarioca <giammattey@gmail.com>
2022-02-20 20:53:17 -0300

    deleting firestore data

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useTheme } from '../hooks/useTheme'
+import Trashcan from '../assets/trashcan.svg'
+import { projectFirestore } from '../firebase/config'
 import './RecipeList.css'
 
 export default function RecipeList({ recipes }) {
@@ -7,6 +9,10 @@ export default function RecipeList({ recipes }) {
 
   if (recipes.length === 0) {
     return <div className='error'>No recipes to load...</div>
+  }
+
+  const handleClick = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete()
   }
 
   return (
@@ -17,6 +23,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className='delete'
+            onClick={() => handleClick(recipe.id)}
+            src={Trashcan}
+            alt='delete icon'
+          />
         </div>
       ))}
     </div>
