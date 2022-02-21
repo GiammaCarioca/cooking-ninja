4d1ddb0c085f9210e6f95950d9f7424dc7d136d1
giammacarioca <giammattey@gmail.com>
2022-02-20 21:02:14 -0300

    real-time collection data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,10 +12,8 @@ export default function Home() {
   useEffect(() => {
     setIsPending(true)
 
-    projectFirestore
-      .collection('recipes')
-      .get()
-      .then((snapshot) => {
+    const unsub = projectFirestore.collection('recipes').onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           setError('No recipes to load')
           setIsPending(false)
@@ -28,11 +26,14 @@ export default function Home() {
           setData(results)
           setIsPending(false)
         }
-      })
-      .catch((err) => {
+      },
+      (err) => {
         setError(err.message)
         setIsPending(false)
-      })
+      }
+    )
+
+    return () => unsub()
   }, [])
 
   return (
